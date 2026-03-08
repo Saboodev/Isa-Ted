f5e14f7e8470c7709ebcad361a3040a7af4214e4
saboodev <sabrina_desantis@hotmail.fr>
2026-03-08 03:42:20 +0100

    finalisation style et compte-à-rebours

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ function toggleMenu() {
     var navMenu = document.querySelector('nav');
     navMenu.classList.toggle('show');
 }
+
+//gestion compte-à-rebours
+const dateMariage = new Date("July 12, 2026 15:00:00").getTime();
+
+const compteur = setInterval(function(){
+
+let maintenant = new Date().getTime();
+let distance = dateMariage - maintenant;
+
+let jours = Math.floor(distance / (1000*60*60*24));
+
+document.getElementById("countdown").innerHTML =
+jours + " jours avant le mariage";
+
+},1000);
